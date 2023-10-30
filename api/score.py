2b1d3ed330ed1ae08cbd6981645c3c1dfb6836f2
7mwang <matthewxpwang@gmail.com>
2023-10-29 22:32:18 -0700

    added saving with usernames, polished

--- api/score.py
+++ api/score.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource
 
-score_api = Blueprint("score_api", __name__, url_prefix = "/api/scores")
+score_api = Blueprint("score_api", __name__, url_prefix="/api/scores")
 api = Api(score_api)
 
 # In-memory data store for game scores
 game_scores = []
+
+# Dictionary to store user-specific data
+user_data = {}
 
 class ScoreAPI(Resource):
     def get(self):
@@ -23,3 +26,26 @@ class ScoreAPI(Resource):
 
 api.add_resource(ScoreAPI, '/')
 
+class UserDataAPI(Resource):
+    def get(self, user_id):
+        if user_id in user_data:
+            return jsonify(user_data[user_id])
+        else:
+            return jsonify(error='User data not found'), 404
+
+    def post(self, user_id):
+        data = request.get_json()
+        user_data[user_id] = {
+            'count': data.get('count', 0),
+            'rate': data.get('rate', 1),
+            'cost': data.get('cost', 100),
+            'clickerCost': data.get('clickerCost', 1000),
+            'clickerCount': data.get('clickerCount', 0),
+            'doubleCount': data.get('doubleCount', 0),
+            'dbCost': data.get('dbCost', 1500),
+            'plusCount': data.get('plusCount', 0),
+        }
+        return jsonify(message='User data stored successfully'), 201
+
+
+api.add_resource(UserDataAPI, '/users/<string:user_id>')
