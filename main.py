210a85925d073f09a2e6b956d4dcc4d16dee2a4a
7mwang <matthewxpwang@gmail.com>
2023-10-30 18:08:19 -0700

    added back cors  import in main

--- main.py
+++ main.py
@@ -61,4 +61,6 @@ def activate_job():  # activate these items
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
+    from flask_cors import CORS
+    cors = CORS(app)
     app.run(debug=True, host="127.0.0.1", port="8640")
