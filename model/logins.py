a74988748e92cbb01ef5dd1c573926fca713d356
7mwang <matthewxpwang@gmail.com>
2023-11-05 18:58:30 -0800

    implemented db for logins

---
+++ model/logins.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+# Connect to the database (create a new one if it doesn't exist)
+import os
+# Get the absolute path to the 'instance' directory
+instance_directory = os.path.abspath('instance')
+
+# Specify the absolute path to the database file
+db_file_path = os.path.join(instance_directory, 'volumes', 'login.db')
+
+# Connect to the database using the absolute path
+conn = sqlite3.connect(db_file_path)
+
+cursor = conn.cursor()
+conn = sqlite3.connect(db_file_path)
+cursor = conn.cursor()
+
+# Create a table for user login data
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
