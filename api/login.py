d24d090893266080e1d9c2992d9a6ecfcc1cd51d
7mwang <matthewxpwang@gmail.com>
2023-11-05 20:13:18 -0800

    deleted database, debugging

--- api/login.py
+++ api/login.py
@@ -1,19 +1,12 @@
-import sqlite3
 from flask import Blueprint, request, jsonify, make_response
 from flask_restful import Resource, Api
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
-# Get the absolute path to the 'instance' directory
-instance_directory = os.path.abspath('instance')
 
-# Specify the absolute path to the database file
-db_file_path = os.path.join(instance_directory, 'volumes', 'scores.db')
-print(db_file_path)
-# Connect to the database using the absolute path
+# In-memory data store for registered users (replace this with a database)
+users = []
+
 login_api = Blueprint("login_api", __name__, url_prefix="/api/login")
 api = Api(login_api)
-
-# ...
 
 class UserRegistration(Resource):
     def post(self):
@@ -24,23 +17,11 @@ class UserRegistration(Resource):
         if not username or not password:
             return make_response(jsonify(error='Username and password are required'), 400)
 
-        # Connect to the database
-        conn = sqlite3.connect(db_file_path)
-        cursor = conn.cursor()
-
-        # Check if the username is already taken
-        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
-        existing_user = cursor.fetchone()
-
-        if existing_user:
-            conn.close()
+        if any(user['username'] == username for user in users):
             return make_response(jsonify(error='Username is already taken'), 409)
 
         hashed_password = generate_password_hash(password)
-        cursor.execute('''INSERT INTO users(username, password) VALUES(?, ?)''', (username, hashed_password))
-        conn.commit()
-        conn.close()
-
+        users.append({'username': username, 'password': hashed_password})
         return make_response(jsonify(message='Registration successful'), 201)
 
 class UserLogin(Resource):
@@ -49,23 +30,15 @@ class UserLogin(Resource):
         username = data.get('username')
         password = data.get('password')
 
-        # Connect to the database
-        conn = sqlite3.connect(db_file_path)
-        cursor = conn.cursor()
-
-        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
-        user = cursor.fetchone()
-        conn.close()
+        user = next((user for user in users if user['username'] == username), None)
 
         if not user:
             return make_response(jsonify(error='User not found'), 400)
 
-        if not check_password_hash(user[2], password):
+        if not check_password_hash(user['password'], password):
             return make_response(jsonify(error='Invalid password'), 409)
 
         return make_response(jsonify(message='Login successful'), 200)
-
-# ...
 
 # Add user-related resources to the blueprint
 api.add_resource(UserRegistration, '/register')
